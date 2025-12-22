4b2f612d49e326999aa68a896decb71402fab9a9
Fuad Elhasan Irfani <hello@feilvan.art>
2025-12-22 20:57:08 +0700

    #5 26/143

--- 5.longest-palindromic-substring.ts
+++ 5.longest-palindromic-substring.ts
@@ -5,68 +5,56 @@
  */
 
 // * TESTCASE
-const t = "babad";
+const t = "aaaa";
 // @lc code=start
 function longestPalindrome(s: string): string {
-  const sArr = s.split("");
-  let longest: string[] = [];
-  let palindromes: string[][] = [];
-  let current: string[] = [];
+  if (s.length === 1) return s;
 
-  function checkNeighbors(s: string[]): string[] {
-    let res: string[] = [];
-    if (s[0] === s[s.length - 1]) {
-      res.concat(s);
-      return res;
+  interface subsIndex {
+    start: number;
+    finish: number;
+  }
+  const sArr = s.split("");
+  let longest: subsIndex = { start: 0, finish: 0 };
+
+  function checkNeighbors(start: number, finish: number): subsIndex {
+    // palindrome
+    if (sArr[start] === sArr[finish]) return { start, finish };
+    // initial loop
+    if (finish - start === 2) {
+      // palindrome left & middle
+      if (sArr[start] === sArr[start + 1]) return { start, finish: start + 1 };
+      // palindrome middle & right
+      if (sArr[start + 1] === sArr[finish]) return { start: start + 1, finish };
     }
-    if (s.length === 3) {
-      if (s[1] === s[0]) {
-        res.push(s[0], s[1]);
-        return res;
-      }
-      if (s[1] === s[2]) {
-        res.push(s[1], s[2]);
-        return res;
-      }
-      return [s[1]];
-    }
-    return s;
+    // not palindrome
+    return { start: start + 1, finish: finish - 1 };
   }
 
   sArr.forEach((v, i, r) => {
-    /*
-    * AN ENGLIGHTENMENT
-    for each,
-    check left and right
-    
-      if both same, then palindrome (odd substring length)
-      if (left || right) === v, also palindrome (even substring length)
-    further check left and right
-    * END OF ENGLIGHTENMENT
-    */
-    let subs: string[] = [];
-    let curLen: number = 0;
-    if (i !== 0 && i !== r.length) {
-      subs.push(r[i - 1], r[i], r[i + 1]);
-      subs = checkNeighbors(subs);
-      curLen = subs.length;
+    let subs: subsIndex = { start: 0, finish: 0 };
+    let start = Math.max(0, i - 1);
+    let finish = Math.min(r.length - 1, i + 1);
+    let res = checkNeighbors(start, finish);
+    console.log(i, res);
+    while (subs.finish - subs.start < res.finish - res.start) {
+      res = checkNeighbors(start, finish);
+      subs = { start: res.start, finish: res.finish };
+      console.log(i, res);
     }
-    if (subs.length > longest.length) {
-      longest = [];
-      longest.concat(subs);
-    }
-    if (subsRes.length > 1) {
-      subsRes = checkNeighbors(subs);
-    }
+    subs = { start: res.start, finish: res.finish };
+    console.log(i, res);
+    if (subs.finish - subs.start > longest.finish - longest.start)
+      longest = { start: subs.start, finish: subs.finish };
   });
-  let out = "";
-  out = longest.join();
+  const out: string = sArr.slice(longest.start, longest.finish + 1).join("");
+  // console.log(out);
   return out;
 }
 // @lc code=end
 longestPalindrome(t);
 
 /*
-57m57s
-31m58s
+137m07s 26/143 cases passed (N/A)
+        (i'm tired. looks like i can't solve this. i feel so dumb.)
 */
